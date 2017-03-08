52b8e7978d4e2536112ab4f1495fc68c3f20bd0d
Ranima <raphaelnmastel3@gmail.com>
2017-03-08 12:04:00 -0800

    started track data

---
+++ csharpEx2/csharpEx2/TrackData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpEx2
+{
+    public struct trackData
+    {
+        public string trackName = string.Format("unknown");
+
+        public string artistName;
+
+        public string albumName;
+        
+        public float trackDuration;
+
+        public int trackNumber;
+
+        public trackData(string track) { }
+        public trackData(string track, float trackDuration) { }
+        public trackData(string track, float trackDuration, string albumName) { }
+        public trackData(string track, float trackDuration, string albumName, string artist) { }
+        public trackData(string track, float trackDuration, string albumName, string artist, int numb) { }
+
+        public static bool operator == (trackData lhs, trackData rhs) { }
+        public static bool operator != (trackData lhs, trackData rhs) { }
+
+        public override bool Equals(object obj) {return base.Equals(obj);}
+        public override int GetHashCode() {return base.GetHashCode();}
+        public override string ToString() {return base.ToString();}
+    }
+
+    class TrackData
+    {
+
+    }
+}
